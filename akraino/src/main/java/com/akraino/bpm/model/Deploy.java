87b967efdc51f34438a42539496da52d525358d8
davidplunkett <dp7642@att.com>
2019-02-28 02:05:43 +0000

    add blueprint parameter to deploy
    Change-Id: I1f9ac491c692ccd8abe7778f8c0933da5b7a1c52
    Signed-off-by: davidplunkett <dp7642@att.com>

--- akraino/src/main/java/com/akraino/bpm/model/Deploy.java
+++ akraino/src/main/java/com/akraino/bpm/model/Deploy.java
@@ -17,162 +17,159 @@
 package com.akraino.bpm.model;
 
 public class Deploy {
-	
-	private String sitename;
-	private String filepath;
-	private String fileparams;
-	private String winscpfilepath;
-	private String winscpfileparams;
-	private String remotserver;
-	private int port;
-	private String username;
-	private String password;
-	private String destdir;
-	private String remotefilename;
-	private String remotefileparams;
-	private String deploymentverifier;
-	private String deploymentverifierfileparams;
-	int noofiterations;
-	int waittime;
-	String postverificationscript;
-	String postverificationScriptparams;
-	
-	
-	
 
-	public String getSitename() {
-		return sitename;
-	}
-	public void setSitename(String sitename) {
-		this.sitename = sitename;
-	}
-	public String getFilepath() {
-		return filepath;
-	}
-	public void setFilepath(String filepath) {
-		this.filepath = filepath;
-	}
-	public String getFileparams() {
-		return fileparams;
-	}
-	public void setFileparams(String fileparams) {
-		this.fileparams = fileparams;
-	}
-	public String getWinscpfilepath() {
-		return winscpfilepath;
-	}
-	public void setWinscpfilepath(String winscpfilepath) {
-		this.winscpfilepath = winscpfilepath;
-	}
-	public String getWinscpfileparams() {
-		return winscpfileparams;
-	}
-	public void setWinscpfileparams(String winscpfileparams) {
-		this.winscpfileparams = winscpfileparams;
-	}
-	public String getDestdir() {
-		return destdir;
-	}
-	public void setDestdir(String destdir) {
-		this.destdir = destdir;
-	}
-	
-	
-	
-	public String getRemotserver() {
-		return remotserver;
-	}
-	public void setRemotserver(String remotserver) {
-		this.remotserver = remotserver;
-	}
-	public int getPort() {
-		return port;
-	}
-	public void setPort(int port) {
-		this.port = port;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getRemotefilename() {
-		return remotefilename;
-	}
-	public void setRemotefilename(String remotefilename) {
-		this.remotefilename = remotefilename;
-	}
-	public String getRemotefileparams() {
-		return remotefileparams;
-	}
-	public void setRemotefileparams(String remotefileparams) {
-		this.remotefileparams = remotefileparams;
-	}
-	public String getDeploymentverifier() {
-		return deploymentverifier;
-	}
-	public void setDeploymentverifier(String deploymentverifier) {
-		this.deploymentverifier = deploymentverifier;
-	}
-	public int getNoofiterations() {
-		return noofiterations;
-	}
-	public void setNoofiterations(int noofiterations) {
-		this.noofiterations = noofiterations;
-	}
-	public int getWaittime() {
-		return waittime;
-	}
-	public void setWaittime(int waittime) {
-		this.waittime = waittime;
-	}
-	
-	public String getDeploymentverifierfileparams() {
-		return deploymentverifierfileparams;
-	}
-	
-	public void setDeploymentverifierfileparams(String deploymentverifierfileparams) {
-		this.deploymentverifierfileparams = deploymentverifierfileparams;
-	}
-	
-	public String getPostverificationscript() {
-		return postverificationscript;
-	}
-	
-	public void setPostverificationscript(String postverificationscript) {
-		this.postverificationscript = postverificationscript;
-	}
-	
-	public String getPostverificationScriptparams() {
-		return postverificationScriptparams;
-	}
-	
-	
-	public void setPostverificationScriptparams(String postverificationScriptparams) {
-		this.postverificationScriptparams = postverificationScriptparams;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "Deploy [sitename=" + sitename + ", filepath=" + filepath + ", fileparams=" + fileparams
-				+ ", winscpfilepath=" + winscpfilepath + ", winscpfileparams=" + winscpfileparams + ", remotserver="
-				+ remotserver + ", port=" + port + ", username=" + username + ", password=" + password + ", destdir="
-				+ destdir + ", remotefilename=" + remotefilename + ", remotefileparams=" + remotefileparams
-				+ ", deploymentverifier=" + deploymentverifier + ", deploymentverifierfileparams="
-				+ deploymentverifierfileparams + ", noofiterations=" + noofiterations + ", waittime=" + waittime
-				+ ", postverificationscript=" + postverificationscript + ", postverificationScriptparams="
-				+ postverificationScriptparams + "]";
-	}
-	
-	
-	
-	
- }
+        private String sitename;
+        private String blueprint;
+        private String filepath;
+        private String fileparams;
+        private String winscpfilepath;
+        private String winscpfileparams;
+        private String remotserver;
+        private int port;
+        private String username;
+        private String password;
+        private String destdir;
+        private String remotefilename;
+        private String remotefileparams;
+        private String deploymentverifier;
+        private String deploymentverifierfileparams;
+        int noofiterations;
+        int waittime;
+        String postverificationscript;
+        String postverificationScriptparams;
+
+
+        public String getSitename() {
+                return sitename;
+        }
+        public void setSitename(String sitename) {
+                this.sitename = sitename;
+        }
+        public String getBlueprint() {
+                return blueprint;
+        }
+        public void setBlueprint(String blueprint) {
+                this.blueprint = blueprint;
+        }
+        public String getFilepath() {
+                return filepath;
+        }
+        public void setFilepath(String filepath) {
+                this.filepath = filepath;
+        }
+        public String getFileparams() {
+                return fileparams;
+        }
+        public void setFileparams(String fileparams) {
+                this.fileparams = fileparams;
+        }
+        public String getWinscpfilepath() {
+                return winscpfilepath;
+        }
+        public void setWinscpfilepath(String winscpfilepath) {
+                this.winscpfilepath = winscpfilepath;
+        }
+        public String getWinscpfileparams() {
+                return winscpfileparams;
+        }
+        public void setWinscpfileparams(String winscpfileparams) {
+                this.winscpfileparams = winscpfileparams;
+        }
+        public String getDestdir() {
+                return destdir;
+        }
+        public void setDestdir(String destdir) {
+                this.destdir = destdir;
+        }
+        public String getRemotserver() {
+                return remotserver;
+        }
+        public void setRemotserver(String remotserver) {
+                this.remotserver = remotserver;
+        }
+        public int getPort() {
+                return port;
+        }
+        public void setPort(int port) {
+                this.port = port;
+        }
+        public String getUsername() {
+                return username;
+        }
+        public void setUsername(String username) {
+                this.username = username;
+        }
+        public String getPassword() {
+                return password;
+        }
+        public void setPassword(String password) {
+                this.password = password;
+        }
+        public String getRemotefilename() {
+                return remotefilename;
+        }
+        public void setRemotefilename(String remotefilename) {
+                this.remotefilename = remotefilename;
+        }
+        public String getRemotefileparams() {
+                return remotefileparams;
+        }
+        public void setRemotefileparams(String remotefileparams) {
+                this.remotefileparams = remotefileparams;
+        }
+        public String getDeploymentverifier() {
+                return deploymentverifier;
+        }
+        public void setDeploymentverifier(String deploymentverifier) {
+                this.deploymentverifier = deploymentverifier;
+        }
+        public int getNoofiterations() {
+                return noofiterations;
+        }
+        public void setNoofiterations(int noofiterations) {
+                this.noofiterations = noofiterations;
+        }
+        public int getWaittime() {
+                return waittime;
+        }
+        public void setWaittime(int waittime) {
+                this.waittime = waittime;
+        }
+
+        public String getDeploymentverifierfileparams() {
+                return deploymentverifierfileparams;
+        }
+
+        public void setDeploymentverifierfileparams(String deploymentverifierfileparams) {
+                this.deploymentverifierfileparams = deploymentverifierfileparams;
+        }
+
+        public String getPostverificationscript() {
+                return postverificationscript;
+        }
+
+        public void setPostverificationscript(String postverificationscript) {
+                this.postverificationscript = postverificationscript;
+        }
+
+        public String getPostverificationScriptparams() {
+                return postverificationScriptparams;
+        }
+
+        public void setPostverificationScriptparams(String postverificationScriptparams) {
+                this.postverificationScriptparams = postverificationScriptparams;
+        }
+
+        @Override
+        public String toString() {
+                return "Deploy [sitename=" + sitename + ", blueprint=" + blueprint + ", filepath=" + filepath + ", fileparams=" + fileparams
+                        + ", winscpfilepath=" + winscpfilepath + ", winscpfileparams=" + winscpfileparams + ", remotserver="
+                        + remotserver + ", port=" + port + ", username=" + username + ", password=" + password + ", destdir="
+                        + destdir + ", remotefilename=" + remotefilename + ", remotefileparams=" + remotefileparams
+                        + ", deploymentverifier=" + deploymentverifier + ", deploymentverifierfileparams="
+                        + deploymentverifierfileparams + ", noofiterations=" + noofiterations + ", waittime=" + waittime
+                        + ", postverificationscript=" + postverificationscript + ", postverificationScriptparams="
+                        + postverificationScriptparams + "]";
+        }
+}
+
